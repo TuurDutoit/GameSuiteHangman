602131e0d67b78b1f69f97747370b8bb51744a05
ABemdt <arne.vandebemdt@student.ucll.be>
2017-05-09 15:26:25 +0200

    Story 09 - Driehoek - added getOmhullende Doener: Arne Denker: Phloy

--- GamesuiteHangman/src/domain/Driehoek.java
+++ GamesuiteHangman/src/domain/Driehoek.java
@@ -1,7 +1,5 @@
 package domain;
 
-
-
 public class Driehoek extends Vorm {
 
 	private Punt hoekpunt1, hoekpunt2, hoekpunt3;
@@ -13,9 +11,8 @@ public class Driehoek extends Vorm {
 	private void setHoekPunten(Punt punt1, Punt punt2, Punt punt3) {
 		if (punt1 == null || punt2 == null || punt3 == null)
 			throw new DomainException("Hoekpunten mogen niet null zijn");
-		else if ((punt2.getX() - punt1.getX())
-				* (punt3.getY() - punt1.getY()) == (punt3.getX() - punt1.getX())
-						* (punt2.getY() - punt1.getY()))
+		else if ((punt2.getX() - punt1.getX()) * (punt3.getY() - punt1.getY()) == (punt3.getX() - punt1.getX())
+				* (punt2.getY() - punt1.getY()))
 			throw new DomainException("Hoekpunten mogen niet op 1 lijn liggen");
 		this.hoekpunt1 = punt1;
 		this.hoekpunt2 = punt2;
@@ -34,15 +31,30 @@ public class Driehoek extends Vorm {
 		return hoekpunt3;
 	}
 
+	public Omhullende getOmhullende(){
+		
+		int yKlein = Math.min(Math.min(hoekpunt1.getY(), hoekpunt2.getY()), Math.min(hoekpunt2.getY(), hoekpunt3.getY()));
+		int yGroot = Math.max(Math.max(hoekpunt1.getY(), hoekpunt2.getY()), Math.max(hoekpunt2.getY(), hoekpunt3.getY()));
+		int xKlein = Math.min(Math.min(hoekpunt1.getX(), hoekpunt2.getX()), Math.min(hoekpunt2.getX(), hoekpunt3.getY()));
+		int xGroot = Math.max(Math.max(hoekpunt1.getX(), hoekpunt2.getX()), Math.max(hoekpunt2.getX(), hoekpunt3.getY()));
+		Punt linksBoven = new Punt(xKlein,yGroot);
+		 
+		int breedte = xGroot - xKlein;
+		int hoogte = yGroot - yKlein;
+		
+		return new Omhullende(linksBoven, breedte, hoogte);
+	}
+
 	@Override
 	public boolean equals(Object obj) {
-		if (this == obj) return true;
-		if (obj == null || getClass() != obj.getClass()) return false;
+		if (this == obj)
+			return true;
+		if (obj == null || getClass() != obj.getClass())
+			return false;
 
 		Driehoek driehoek = (Driehoek) obj;
 
-		return hoekpunt1 == driehoek.getHoekPunt1()
-				&& hoekpunt2 == driehoek.getHoekPunt2()
+		return hoekpunt1 == driehoek.getHoekPunt1() && hoekpunt2 == driehoek.getHoekPunt2()
 				&& hoekpunt3 == driehoek.getHoekPunt3();
 	}
 
@@ -51,8 +63,4 @@ public class Driehoek extends Vorm {
 		return "Driehoek: " + hoekpunt1 + " - " + hoekpunt2 + " - " + hoekpunt3;
 	}
 
-	
-
-
 }
-
