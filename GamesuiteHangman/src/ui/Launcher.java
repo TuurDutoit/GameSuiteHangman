872d21c80ac603a948103aeff6b28ff1e8bf6b14
Tuur Dutoit <me@tuurdutoit.be>
2017-05-09 10:49:29 +0200

    Story 03 - Cirkel Denker: Arne, Doener: Tuur
     - Updated Launcher
    
    => Story afgewerkt

--- GamesuiteHangman/src/ui/Launcher.java
+++ GamesuiteHangman/src/ui/Launcher.java
@@ -2,23 +2,32 @@ package ui;
 
 import javax.swing.JOptionPane;
 
+import domain.Cirkel;
 import domain.Punt;
 import domain.Speler;
 
 public class Launcher {
 
 	public static void main(String[] args) {
-		String naam = JOptionPane.showInputDialog("Welkom! \nHoe heet je?");
-		Speler speler = new Speler(naam);
+		try {
+			String naam = JOptionPane.showInputDialog("Welkom! \nHoe heet je?");
+			Speler speler = new Speler(naam);
+			
+			JOptionPane.showMessageDialog(null, "... heeft als score: "+speler.getScore(), speler.getNaam(), JOptionPane.INFORMATION_MESSAGE);
 		
-		JOptionPane.showMessageDialog(null, "... heeft als score: "+speler.getScore(), speler.getNaam(), JOptionPane.INFORMATION_MESSAGE);
-	
-		int x = Integer.parseInt(JOptionPane.showInputDialog("x coordinaat van het punt:"));
-		int y = Integer.parseInt(JOptionPane.showInputDialog("y coordinaat van het punt:"));
 		
-		Punt punt = new Punt(x, y);
-		
-		JOptionPane.showMessageDialog(null, "U heeft een correct punt aangemaakt: " + punt.toString() ,speler.getNaam(), JOptionPane.INFORMATION_MESSAGE);
+			int x = Integer.parseInt(JOptionPane.showInputDialog("x coordinaat van het punt:"));
+			int y = Integer.parseInt(JOptionPane.showInputDialog("y coordinaat van het punt:"));
+			int r = Integer.parseInt(JOptionPane.showInputDialog("Radius van de cirkel:"));
+			
+			Punt punt = new Punt(x, y);
+			Cirkel cirkel = new Cirkel(punt, r);
+			
+			JOptionPane.showMessageDialog(null, "U heeft een correcte cirkel aangemaakt: " + cirkel.toString(), speler.getNaam(), JOptionPane.INFORMATION_MESSAGE);
+		}
+		catch(Exception exc) {
+			JOptionPane.showMessageDialog(null,  "Er is een fout opgetreden: " + exc.getMessage(), "Fout", JOptionPane.ERROR_MESSAGE);
+		}
 	}
-
+	
 }
