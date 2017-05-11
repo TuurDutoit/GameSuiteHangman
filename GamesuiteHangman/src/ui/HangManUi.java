3287c7692750bfbe65e2956315860fa71ba9f27b
ABemdt <arne.vandebemdt@student.ucll.be>
2017-05-11 11:37:51 +0200

    Story 13 - Added db, DbException, WoordenLezer - Updated Launcher, HangmanUI Denker: Arne Doener: Jonas

--- GamesuiteHangman/src/ui/HangManUi.java
+++ GamesuiteHangman/src/ui/HangManUi.java
@@ -4,44 +4,47 @@ import javax.swing.JOptionPane;
 
 import domain.HintWoord;
 import domain.Speler;
+import domain.WoordenLijst;
 
 public class HangManUi {
 	private Speler speler;
 	private String title;
-	
-	public HangManUi(Speler speler) {
-		if(speler == null) {
+	private WoordenLijst lijst;
+
+	public HangManUi(Speler speler, WoordenLijst lijst) {
+		if (speler == null) {
 			throw new UiException("Speler mag niet null zijn");
 		}
-		
+
 		this.speler = speler;
 		this.title = "HangMan - " + speler.getNaam();
+		this.lijst = lijst;
 	}
-	
+
 	public void play() {
-		HintWoord woord = new HintWoord("test");
+		HintWoord woord = new HintWoord(this.lijst.getRandomWoord());
 		boolean juist = false;
 		char letter = 0;
-		
-		while(!woord.isGeraden()) {
+
+		while (!woord.isGeraden()) {
 			String content = "";
-			
-			if(letter != 0) {
-				if(juist) {
+
+			if (letter != 0) {
+				if (juist) {
 					content += letter + " is juist geraden!\n\n";
-				}
-				else {
+				} else {
 					content += letter + " zit niet in het woord (of heb je al eens geprobeerd)...\n\n";
 				}
 			}
-			
+
 			content += "Raad een letter:\n\n" + woord;
 			String input = JOptionPane.showInputDialog(null, content, title, JOptionPane.INFORMATION_MESSAGE);
 			letter = input.charAt(0);
 			juist = woord.raad(letter);
 		}
-		
-		JOptionPane.showMessageDialog(null, "U heeft het woord geraden!\n\n>>> " + woord + " <<<", "GEWONNEN!", JOptionPane.INFORMATION_MESSAGE);
+
+		JOptionPane.showMessageDialog(null, "U heeft het woord geraden!\n\n>>> " + woord + " <<<", "GEWONNEN!",
+				JOptionPane.INFORMATION_MESSAGE);
 	}
-	
+
 }
